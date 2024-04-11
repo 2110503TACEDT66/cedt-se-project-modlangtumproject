cfc7d1dc52294d05861929ce0262685f1fe5e6bc
MoosaTae <sadhis.tae@gmail.com>
2024-04-12 03:21:33 +0700

    Add nodemailer for email and update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ const {
   getMe,
   logout,
 } = require('../controllers/userController');
+import { forgetPassword, resetPassword } from '../controllers/forgetPasswordController';
 
 const router = express.Router();
 
@@ -15,5 +16,8 @@ router.post('/register', upload.single('profile'), register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
+router.post('/forget-password', forgetPassword);
+router.post('/reset-password/:resetToken', resetPassword);
+
 
 module.exports = router;
