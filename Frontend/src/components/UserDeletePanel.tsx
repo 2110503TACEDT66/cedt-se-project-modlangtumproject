7dbeae08889e4e8fc14ae638968db034a3d7c633
MoosaTae <sadhis.tae@gmail.com>
2024-04-19 01:01:43 +0700

    add delete fetch backend

--- Frontend/src/components/UserDeletePanel.tsx
+++ Frontend/src/components/UserDeletePanel.tsx
@@ -1,39 +1,46 @@
+'use client';
+
 import React from 'react';
 import deleteAccount from '@/libs/deleteAccount';
 import getUserProfile from '@/libs/getUserProfile';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { useSession, signOut } from 'next-auth/react';
 
-const UserDeletePanel: React.FC =  async () => {
+const UserDeletePanel: React.FC = () => {
+  const { data: session } = useSession();
+  const handleDeleteUser = async () => {
+    try {
+      if (!session || !session.user.token) return;
 
-    const session = await getServerSession(authOptions);
-    if (!session || !session.user.token) return null;
-    const profile = await getUserProfile(session.user.token);
+      const profile = await getUserProfile(session.user.token);
+      const id = profile.data._id;
+      const token = session.user.token;
+      const response = await deleteAccount({ id, token});
 
-    const handleDeleteUser = async () => {
+      if (!response.success) {
+        throw new Error('Network response was not ok');
+      }
+      await signOut({ redirect: false });      
 
-        try {
-
-            const id = profile.data.id;
-            const token = session.user.token;
-            await deleteAccount ({ id, token});
-
-        } catch (error) {
-            console.error('Error deleting account:', error);
-
-        }
+      alert('Account deleted');
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Error deleting account:', error);
     }
+  };
 
-    return (
-        <div className="z-50 space-y-2 p-20 items-center flex flex-col">
-            <div className="flex flex-col items-center justify-between space-y-3">
-                <div className="p-5 text-5xl">Security</div>
-            </div>
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handleDeleteUser}>
-                Delete User
-            </button>
-        </div>
-    );
+  return (
+    <div className="z-50 flex flex-col items-center space-y-2 p-20">
+      <div className="flex flex-col items-center justify-between space-y-3">
+        <div className="p-5 text-5xl">Security</div>
+      </div>
+      <button
+        className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+        onClick={handleDeleteUser}
+      >
+        Delete User
+      </button>
+    </div>
+  );
 };
 
 export default UserDeletePanel;
--- Frontend/src/libs/deleteAccount.tsx
+++ Frontend/src/libs/deleteAccount.tsx
@@ -2,8 +2,8 @@ export default async function deleteAccount({
     id, 
     token,
   }: {
-    id: string
-    token: string;
+    id: string,
+    token: string
   }) {
     const response = await fetch(
       `https://modlangtum-api.vercel.app/auth/delete/${id}`,
