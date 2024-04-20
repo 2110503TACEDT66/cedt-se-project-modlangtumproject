e943c54d88ede7c953d0e7a4a6a79084e584b7ab
datswhaaan <145758336+datswhaaan@users.noreply.github.com>
2024-04-20 12:53:12 +0000

    fix 'upload resume'

--- Backend/middleware/uploadResume.js
+++ Backend/middleware/uploadResume.js
@@ -2,9 +2,9 @@ const path = require('path');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'resumes/');
-  },
+  // destination: function (req, file, cb) {
+  //   cb(null, 'resumes/');
+  // },
   filename: function (req, file, cb) {
     let ext = path.extname(file.originalname);
     cb(null, Date.now() + ext);
