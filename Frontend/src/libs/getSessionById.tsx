eeda7b29ba3730bf51557c3c59a3d8532f80fb72
Jirayu Khunrak <136585173+Unnyaamaanee@users.noreply.github.com>
2024-04-25 12:31:50 +0700

    some vercel problem

--- Frontend/src/libs/getSessionById.tsx
+++ Frontend/src/libs/getSessionById.tsx
@@ -1,7 +1,8 @@
 export default async function getSession(token: string, id: string) {
   const response = await fetch(
-    `https://modlangtum-api.vercel.app/sessions/${id}`,
-    {
+    `https://modlangtum-api.vercel.app/sessions/${id}`
+    //`http://localhost:5000/sessions/${id}
+    ,{
       method: 'GET',
       headers: {
         authorization: `Bearer ${token}`,
