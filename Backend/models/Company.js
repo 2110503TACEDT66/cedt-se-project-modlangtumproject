331ec6f2f43d29e2c76cdd2b54dc3b7a89ddc922
earnt <tinnaearn@gmail.com>
2024-04-28 01:38:30 +0700

    create relationship between company and job

--- Backend/models/Company.js
+++ Backend/models/Company.js
@@ -39,12 +39,21 @@ CompanySchema.virtual('sessions', {
   justOne: false,
 });
 
+CompanySchema.virtual('job', {
+  ref: 'Job',
+  localField: '_id',
+  foreignField: 'job_id',
+  justOne: false,
+});
+
 CompanySchema.pre(
   'deleteOne',
   { document: true, query: false },
   async function (next) {
     console.log(`Sessions being removed from company ${this._id}`);
     await this.model('Session').deleteMany({ company: this._id });
+    console.log(`Job being removed from company ${this._id}`);
+    await this.model('Job').deleteMany({ company: this._id });
     next();
   },
 );
