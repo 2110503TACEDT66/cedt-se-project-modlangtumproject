11417b159d0e3b468e1eb52a5dc489f57aa281d2
naphat <naphatchartwanchai@gmail.com>
2024-05-01 00:18:26 +0700

    fix cypress

--- Cypress/cypress/e2e/test.cy.js
+++ Cypress/cypress/e2e/test.cy.js
@@ -25,4 +25,40 @@ describe('Test visit my web', () => {
     cy.get("button").contains("View Available Job").click();
     cy.get("button").contains("Apply").click();
   })
+
+  it('User2-1', ()=> {
+    cy.visit("/");
+    cy.visit("/auth/login");
+    cy.get("input[name=email]").type("admin@gmail.com");
+    cy.get("input[name=password]").type("123456");
+    cy.get("button").contains("Sign in with Credentials").click();
+    cy.url().should("eq","http://localhost:3000/");
+    cy.visit()
+  })
+
+  it('User2-3', () => {
+    cy.visit("/");
+    cy.visit("/auth/login");
+    cy.get("input[name=email]").type("pppp@gmail.com");
+    cy.get("input[name=password]").type("123456");
+    cy.get("button").contains("Sign in with Credentials").click();
+    cy.url().should("eq", "http://localhost:3000/");
+    cy.visit("/company");
+    cy.visit("/company/65e3428465164ca39be76934");
+    cy.get("button").contains("View Available Job").click();
+
+  })
+
+  it('User2-2', ()=> {
+    cy.visit("/");
+    cy.visit("/auth/login");
+    cy.get("input[name=email]").type("admin@gmail.com");
+    cy.get("input[name=password]").type("123456");
+    cy.get("button").contains("Sign in with Credentials").click();
+    cy.url().should("eq","http://localhost:3000/");
+    cy.visit("/company");
+    cy.visit("/company/65e3428465164ca39be76934");
+    cy.get("button").contains("View Available Job").click();
+    cy.get("button").contains("Delete").click();
+  })
 })
