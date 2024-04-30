53d083316be73ab8930f6620474ce9c83563026f
Jirayu Khunrak <136585173+Unnyaamaanee@users.noreply.github.com>
2024-05-01 02:48:19 +0700

    Update t1.cy.js

--- Cypress/cypress/e2e/t1.cy.js
+++ Cypress/cypress/e2e/t1.cy.js
@@ -29,11 +29,11 @@ describe('Test1 visit my web', () => {
     cy.wait(500);
     cy.visit("/auth/register");
     cy.wait(500);
-    cy.get("input[name=name]").type("GemKa3");
+    cy.get("input[name=name]").type("Jirayu");
     cy.wait(500);
-    cy.get("input[name=email]").type("gemKaa3@gmail.com");
+    cy.get("input[name=email]").type("Jirayu@gmail.com");
     cy.wait(500);
-    cy.get("input[name=tel]").type("0019283749");
+    cy.get("input[name=tel]").type("0019283678");
     cy.wait(500);
     cy.get("input[name=password]").type("123456");
     cy.wait(500);
@@ -44,7 +44,7 @@ describe('Test1 visit my web', () => {
     cy.get("button").contains("Submit").click();
     cy.wait(500);
     cy.url().should("eq", "http://localhost:3000/api/auth/signin");
-    cy.get("input[name=email]").type("gemKaa3@gmail.com");
+    cy.get("input[name=email]").type("Jirayu@gmail.com");
     cy.wait(500);
     cy.get("input[name=password]").type("123456");
     cy.wait(500);
