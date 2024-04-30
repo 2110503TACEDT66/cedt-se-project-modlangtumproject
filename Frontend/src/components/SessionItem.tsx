3a561f6c2ec51ac083eb5b4ca93ce88e27a55d50
Jirayu Khunrak <136585173+Unnyaamaanee@users.noreply.github.com>
2024-05-01 00:49:07 +0700

    delete import that not use

--- Frontend/src/components/SessionItem.tsx
+++ Frontend/src/components/SessionItem.tsx
@@ -2,9 +2,6 @@
 import { useRouter} from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import getUserProfile from '@/libs/getUserProfile';
-import getJob from '@/libs/getJob';
-import { useEffect, useState } from 'react';
 
 export default function SessionItem({
   id,
